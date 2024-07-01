633c9e52f95ca8aaf556f9af06ff34c7f6c3a1ea
Dranell <dranellwilson!@gmail.com>
2024-07-01 10:23:05 -0500

    Created a database in MySQL using python

---
+++ create_data_base.py
@@ -0,0 +1,2 @@
+cape_local_medical_center = "create database cape_local_medical_center"
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import mysql.connector
 from mysql.connector import Error
 
+import create_data_base as cdb
 
 def createServerConnection(host_name,user_name,user_password):
     connection = None
@@ -16,4 +17,14 @@ def createServerConnection(host_name,user_name,user_password):
         print(f"Error:{err}")
     return connection
 
-createServerConnection("localhost","root","student")
+def create_database(connection,query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        #cursor is created by the connection to mySQL. Think of the cursor as the blinking box ready to be used by the user. We are telling the cursor to
+        #execute the query.
+        print("Database created successfully!")
+    except Error as err:
+        print(f"Error: {err}")
+conntection = createServerConnection("localhost","root","student")
+create_database(conntection,cdb.cape_local_medical_center )
